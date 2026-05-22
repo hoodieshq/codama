21c05964dca6f2fcfb0a49f0f43e79a16e671cee
Mikhail <mikhail@hoodies.company>
2026-05-22 17:13:59 +0300

    fix: remove generateInstructionBuildersMap from public api

--- packages/dynamic-instructions/src/codegen/index.ts
+++ packages/dynamic-instructions/src/codegen/index.ts
@@ -1,3 +1,2 @@
-export { generateInstructionBuildersMap } from './generate-instruction-builder';
 export { generateSignerTypes } from './generate-signer-types';
 export { generateTypes } from './generate-types';
